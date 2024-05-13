70a91fa5d443ba32b3f4a4fca4fbd580adbe8356
Yulianna Garcia <yuliannagarcia01@gmail.com>
2024-05-13 19:35:11 +0100

    edited socials

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -3,7 +3,7 @@ import './Socials.css'
 import YG from '../../Assets/yg.png';
 import {BsLinkedin} from 'react-icons/bs';
 import {BsGithub} from 'react-icons/bs';
-import {BsInstagram} from 'react-icons/bs';
+
  
 const Socials = () => {
   return ( 
@@ -11,8 +11,7 @@ const Socials = () => {
       <div className='socials'>
         <a href="https://www.linkedin.com/in/yulianna-garcia-37919a1a2" target='_blank' rel="noreferrer"><BsLinkedin/></a>
         <a href="https://github.com/yuliannagarcia" target='_blank' rel="noreferrer"><BsGithub/></a>
-        <a href="https://instagram.com/designwithyulianna?igshid=MTIyMzRjYmRlZg==" target='_blank' rel="noreferrer"><BsInstagram/></a>
-        <img src={YG} alt="" className='logo' href="#Home"/>
+        <img src={YG} alt="" className='logo'/>
       </div> 
     </>
   )
